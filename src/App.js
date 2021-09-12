7e4527d3476e5b11a1310851dca156c138ad8d04
mrparkersson <amoparkerprince78@gmail.com>
2021-09-12 09:12:54 +0000

    music player

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// ADDING COMPONENTS
+
+import Player from "./Components/Player";
+import Song from "./Components/Song";
+
+function App() {
+  return (
+    <div className="App">
+      <Song />
+      <Player />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Player.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Player = () => {
+  return (
+    <div className="player">
+      <div className="time-control">
+        <p>Start Time</p>
+        <input type="range" />
+        <p>End Time</p>
+      </div>
+      <div className="play-control"></div>
+    </div>
+  );
+};
+
+export default Player;
